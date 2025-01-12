37dd867f7b9876f4471cc1f5ba9fbc1900539321
Hammad0010 <hammadmusawar@gmail.com>
2025-01-12 20:51:52 +0500

    Test updated workflow

--- src/components/navigation-bar/navigation-bar.jsx
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -30,7 +30,7 @@ export const NavigationBar = ({ user, onLoggedOut }) => {
                                     to="/recommendations" 
                                     className={location.pathname === '/recommendations' ? 'active' : ''}
                                 >
-                                    Recommendation
+                                    Recommendations
                                 </Nav.Link>
                             </>
                         )}
